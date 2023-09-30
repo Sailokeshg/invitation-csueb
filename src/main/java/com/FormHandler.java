d86aa0643eabf29021f0afa8db082c7529f16c49
Sailokeshg <sailokeshreddyg@gmail.com>
2023-09-29 23:02:28 -0700

    implemented three screens with jsp

--- src/main/java/com/FormHandler.java
+++ src/main/java/com/FormHandler.java
@@ -1,21 +1,19 @@
 package com;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.logging.*;
-
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet("/FormHandler")
 public class FormHandler extends HttpServlet{
-	
+
 	private static final long serialVersionUID = 1L;
-	int attendCount = 0;
-	int notAttendCount = 0;
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 		       throws ServletException, IOException
@@ -23,21 +21,23 @@ public class FormHandler extends HttpServlet{
 		response.setContentType("text/html");
 		
 		PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession();
+
+        Integer attendCount = (Integer) session.getAttribute("attendCount");
+        if (attendCount == null) {
+            attendCount = 0;
+        }
+
+        Integer notAttendCount = (Integer) session.getAttribute("notAttendCount");
+        if (notAttendCount == null) {
+            notAttendCount = 0;
+        }
 		
 		String eventName = request.getParameter("eventName");
         String eventDate = request.getParameter("eventDate");
         String eventTime = request.getParameter("eventTime");
         String eventLocation = request.getParameter("eventLocation");
         String eventDescription = request.getParameter("eventDescription");
-        String confirmationChoice = request.getParameter("attendance");
-              
-        if (confirmationChoice != null) {
-            if (confirmationChoice.equals("attend")) {
-                attendCount++;
-            } else if (confirmationChoice.equals("not-attend")) {
-                notAttendCount++;
-            }
-        }
         
         out.println("<!DOCTYPE html>");
         out.println("<html lang=\"en\">");
@@ -45,69 +45,43 @@ public class FormHandler extends HttpServlet{
         out.println("    <title>Event Information</title>");
         out.println("</head>");
         out.println("<body>");
+        out.println("<form method=\"post\" action=\"FormHandler\">");
         out.println("    <h1>Event Information</h1>");
         out.println("    <p>Event Name: " + eventName + "</p>");
         out.println("    <p>Event Date: " + eventDate + "</p>");
         out.println("    <p>Event Time: " + eventTime + "</p>");
         out.println("    <p>Event Location: " + eventLocation + "</p>");
         out.println("    <p>Event Description: " + eventDescription + "</p>");
-        out.println("    <p>Total Attending: " + attendCount + "</p>");
-        out.println("    <p>Total Not Attending: " + notAttendCount + "</p>");
+        out.println("    <button type=\"submit\" name=\"attendance\" value=\"attend\">Attend</button>");
+        out.println("    <button type=\"submit\" name=\"attendance\" value=\"not-attend\">Not Attend</button>");
+        out.println("</form>");
+        out.println("</br>");
+        out.println("<form method=\"GET\" action=\"RestartHandler\">");
+        out.println("    <button type=\"submit\">Restart</button>");
+        out.println("</form>");
         out.println("</body>");
         out.println("</html>");
-        out.close();
+
+        String confirmationChoice = request.getParameter("attendance");
         
-		}
-	
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		
-		response.setContentType("text/html");
-		
-		PrintWriter out  = response.getWriter();
-		
-		out.println("<!DOCTYPE html>");
-        out.println("<html lang=\"en\">");
-        out.println("<head>");
-        out.println("    <title>Event Invitation Page</title>");
-        out.println("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
-        out.println("    <link rel=\"stylesheet\" href=\"styles.css\">");
-        out.println("    <script type=\"text/javascript\" src=\"/validator.js\"></script>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("    <div class=\"container\">");
-        out.println("        <h1>Event Invitation</h1>");
-        out.println("        <p>Kindly fill in the details below and let us know if you will be attending the event.</p>");
-        out.println("        <form id=\"eventForm\" method=\"POST\" action=\"FormHandler\">");
-        out.println("            <label for=\"eventName\">Event Name:</label>");
-        out.println("            <input type=\"text\" id=\"eventName\" name=\"eventName\">");
-        out.println("            <label for=\"eventDate\">Event Date:</label>");
-        out.println("            <input type=\"text\" id=\"eventDate\" name=\"eventDate\" placeholder=\"MM/DD/YYYY\">");
-        out.println("            <label for=\"eventTime\">Event Time:</label>");
-        out.println("            <input type=\"text\" id=\"eventTime\" name=\"eventTime\">");
-        out.println("            <label for=\"eventLocation\">Event Location:</label>");
-        out.println("            <input type=\"text\" id=\"eventLocation\" name=\"eventLocation\">");
-        out.println("            <label for=\"eventDescription\">Event Description:</label>");
-        out.println("            <textarea id=\"eventDescription\" name=\"eventDescription\" rows=\"4\" wrap maxlength=\"50px\"></textarea>");
-        out.println("            <div class=\"attendance-buttons\">");
-        out.println("                <div class=\"attend-button\">");
-        out.println("                    <label for=\"attendance\">");
-        out.println("                        <input type=\"radio\" name=\"attendance\" value=\"attend\">Attend");
-        out.println("                    </label>");
-        out.println("                </div>");
-        out.println("                <div class=\"notattend-button\">");
-        out.println("                    <label for=\"attendance\">");
-        out.println("                        <input type=\"radio\" name=\"attendance\" value=\"not-attend\">Not Attend");
-        out.println("                    </label>");
-        out.println("                </div>");
-        out.println("            </div>");
-        out.println("            <div class=\"submit-button\">");
-        out.println("                <button type=\"submit\" style=\"margin-top: 10px; margin-right: 10%;\">Submit</button>");
-        out.println("            </div>");
-        out.println("        </form>");
-        out.println("    </div>");
-        out.println("</body>");
-        out.println("</html>");
+        if (confirmationChoice != null) {
+            if (confirmationChoice.equals("attend")) {
+                attendCount++;
+            } else if (confirmationChoice.equals("not-attend")) {
+                notAttendCount++;
+            }
+        
+            session.setAttribute("attendCount", attendCount);
+            session.setAttribute("notAttendCount", notAttendCount);
+
+            RequestDispatcher dispatcher = request.getRequestDispatcher("confirmationscreen.jsp");
+            dispatcher.forward(request, response);
+        }
+        
         out.close();
-		
+		}
 	}
-}
+	
+
+
+
---
+++ src/main/webapp/eventform.jsp
@@ -0,0 +1,37 @@
+<%@ page import="java.util.*" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Event Invitation Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <script type="text/javascript" src="validator.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h1>Event Invitation</h1>
+        <p>Kindly fill in the details below and let us know if you will be attending the event.</p>
+       
+        <form id="eventForm" action="FormHandler" method="POST">
+            <label for="eventName">Event Name:</label>
+            <input type="text" id="eventName" name="eventName">
+            
+            <label for="eventDate">Event Date:</label>
+            <input type="text" id="eventDate" name="eventDate" placeholder="MM/DD/YYYY">
+            
+            <label for="eventTime">Event Time:</label>
+            <input type="text" id="eventTime" name="eventTime">
+            
+            <label for="eventLocation">Event Location:</label>
+            <input type="text" id="eventLocation" name="eventLocation">
+            
+            <label for="eventDescription">Event Description:</label>
+            <textarea id="eventDescription" name="eventDescription" rows="4"></textarea>
+
+            <div class="submit-button">
+                <button type="submit" style="margin-top: 10px; margin-right: 10%;">Submit</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
